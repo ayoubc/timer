b3ed7e5f7454a92aa78b5695adfdab733a868467
ayoubc <ayoubch807@gmail.com>
2020-05-02 18:57:12 +0000

    add css and click handlers

--- src/App.js
+++ src/App.js
@@ -4,45 +4,75 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-
+    
     constructor() {
         super();
         this.state = {
-            date: new Date(),
             minutes: 0,
             seconds: 0,
-            hours: 0
+            hours: 0,
+            intervalId: null,
+            actionName: "start"
         }
     }
 
-    componentDidMount() {
-        setInterval(() => {
-            let { minutes, seconds, hours} = this.state;
-            seconds ++;
-            minutes += (seconds >= 60);
-            seconds %= 60;
-            hours += (minutes >= 60);
-            minutes %= 60;
-            this.setState({
-                minutes: minutes,
-                seconds: seconds,
-                hours: hours
-            })
-        }, 1000);
+    startTimer = () => {
+        this.setState({
+            actionName: "stop",
+            intervalId: setInterval(() => {
+                let { minutes, seconds, hours } = this.state;
+                seconds++;
+                minutes += (seconds >= 60);
+                seconds %= 60;
+                hours += (minutes >= 60);
+                minutes %= 60;
+                this.setState({
+                    minutes: minutes,
+                    seconds: seconds,
+                    hours: hours
+                })
+            }, 1000)
+        })
     }
-    
-    displayTime({hours, minutes, seconds}) {
+
+    stopTimer = () => {
+        clearInterval(this.state.intervalId);
+        this.setState({
+            actionName: "start"
+        })
+    }
+
+    handelClick = () => {
+        this.state.actionName === "start" ? this.startTimer() : this.stopTimer();
+    }
+
+    restTimer = () => {
+        this.stopTimer();
+        this.setState({
+            minutes: 0,
+            seconds: 0,
+            hours: 0,
+        })
+    }
+
+    displayTime({ hours, minutes, seconds }) {
 
         if (hours.toString().length <= 1) hours = "0" + hours;
         if (minutes.toString().length <= 1) minutes = "0" + minutes;
         if (seconds.toString().length <= 1) seconds = "0" + seconds;
         return hours + ":" + minutes + ":" + seconds
     }
+
     render() {
-         
+
         return (
-            <div>
-               {this.displayTime(this.state)}
+            <div className="timer">
+                <span> {this.displayTime(this.state)} </span>
+                <hr/>
+                <div>
+                    <button onClick={this.handelClick}>{this.state.actionName}</button>
+                    <button onClick={this.restTimer}>reset</button>
+                </div>
             </div>
         )
     }
